74531e0ac15c3b64d3b077e4a43461ca4079431d
jb <jbmuisha@gmail.com>
2026-03-04 17:05:07 +0400

    student

--- frontend/app/dashboard/student/page.tsx
+++ frontend/app/dashboard/student/page.tsx
@@ -1,1 +1,9 @@
-Student Dashboard
+"use client";
+export default function studentDashboard() {
+  return (
+    <div className="dashboard-content">
+      <h1>Student Dashboard</h1>
+      <p>Welcome to your dashboard! Here you can view your courses, schedule, and reports.</p>
+    </div>
+  );
+}
